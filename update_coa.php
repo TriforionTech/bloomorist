8c76dd6704ca20db2fd2349fc4d0ff335272c267
Falachul Akhadihima Ibrahaical <falachul.ibrahaical@gmail.com>
2026-09-15 12:03:05 +0700

    add fitur filter range date and costumer type

--- update_coa.php
+++
@@ -1,8 +0,0 @@
-<?php
-$c = file_get_contents('app/Services/AccountingService.php');
-$c = str_replace("'1010'", "'1101'", $c);
-$c = str_replace("'4010'", "'4101'", $c);
-$c = str_replace("'1030'", "'1104'", $c);
-$c = str_replace("'5010'", "'6106'", $c);
-file_put_contents('app/Services/AccountingService.php', $c);
-echo "Updated COA codes successfully.\n";
